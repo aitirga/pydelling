4d3936aaa2c3fa140b02f7e11c1b5b12693fdfea
aitorig <aitor.iraola@amphos21.com>
2020-03-05 11:49:42 +0100

    Added hdf5 writer to Interpolator class

--- PyFLOTRAN/interpolation/SparseDataInterpolator.py
+++ PyFLOTRAN/interpolation/SparseDataInterpolator.py
@@ -3,7 +3,9 @@ Interpolates a given set of points into a PFLOTRAN mesh
 """
 from .BaseInterpolator import BaseInterpolator
 from scipy.interpolate import griddata
+import h5py
 import numpy as np
+import os
 
 class SparseDataInterpolator(BaseInterpolator):
     def interpolate(self, **kwargs):
@@ -14,5 +16,19 @@ class SparseDataInterpolator(BaseInterpolator):
         temp_array = np.reshape(self.interpolated_data, (self.interpolated_data.shape[0], 1))
         return np.concatenate((self.mesh, temp_array), axis=1)
 
+    def dump_to_hdf5(self, filename=None, var_name=None, data=None):
+        """
+        Dumps the data into HDF5 format
+        :return:
+        """
+        assert hasattr(self, "id_data"), "Unstructured ID data needs to be provided"
+        if not os.path.exists(filename):
+            tempfile = h5py.File(filename, "w")
+            tempfile.close()
+        with h5py.File(filename, "r+") as tempfile:
+            if not "Cell Ids" in list(tempfile):
+                tempfile.create_dataset("Cell Ids", data=self.id_data, dtype=np.int32)
+            print(self.interpolated_data)
+            tempfile.create_dataset(var_name, data=self.interpolated_data)
 
 
