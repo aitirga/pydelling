8f62354582ccbde2aa9da7ca83e038ad29622dbc
paulabruch <paula.bruch@amphos21.com>
2021-02-17 12:46:21 +0100

    feat: add Clip class to ParaviewProcessor

---
+++ PyFLOTRAN/paraview_processor/filters/ClipFilter.py
@@ -0,0 +1,20 @@
+from PyFLOTRAN.paraview_processor.filters import BaseFilter
+try:
+    from paraview.simple import *
+except:
+    pass
+
+
+class ClipFilter(BaseFilter):
+    """
+    This class implements the Paraview Integrate Variables filter
+    """
+    filter_type: str = "Clip"
+    counter: int = 0
+
+    def __init__(self, input_filter, name):
+        super().__init__(name=name)
+        ClipFilter.counter += 1
+        self.filter = Clip(Input=input_filter)
+
+
