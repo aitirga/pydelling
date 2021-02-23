9870a68920bed90566156ce43fd7b6c4532e0771
aitirga <aitirga@gmail.com>
2021-02-23 17:50:34 +0100

    style: fix typo

--- PyFLOTRAN/paraview_processor/filters/SaveDataFilter.py
+++ PyFLOTRAN/paraview_processor/filters/SaveDataFilter.py
@@ -9,7 +9,7 @@ class SaveDataFilter(BaseFilter):
     """
     This class implements the Save Data filter
     """
-    filter_type: str = "Sava_data"
+    filter_type: str = "Save_data"
     counter: int = 0
 
     def __init__(self, filename, proxy, point_data_arrays, cell_data_arrays, name):
