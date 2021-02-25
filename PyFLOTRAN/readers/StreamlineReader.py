5f43e92bef3e27516cb0722adb9ad41ae25f3c62
aitirga <aitirga@gmail.com>
2021-02-25 21:02:29 +0100

    feat: add method to compute beta at the outlet

--- PyFLOTRAN/readers/StreamlineReader.py
+++ PyFLOTRAN/readers/StreamlineReader.py
@@ -40,7 +40,8 @@ class StreamlineReader(BaseReader):
     def compute_arrival_times(self, reason_of_termination=None) -> pd.Series:
         """
         This method computes the arrival times of the streamlines
-        Returns: An pd.Series object containing the arrival times of the streamlines
+        Returns:
+             A pd.Series object containing the arrival times of the streamlines
         """
         logger.info("Computing arrival times of the streamlines")
         reason_of_termination = reason_of_termination if reason_of_termination else config.streamline_reader.reason_of_termination
@@ -49,6 +50,17 @@ class StreamlineReader(BaseReader):
             temp_df =temp_df.filter(lambda x: x["ReasonForTermination"].max() == reason_of_termination)
         temp_series: pd.Series = temp_df.groupby("SeedIds").max()["IntegrationTime"]
         return temp_series
+
+    def compute_beta(self):
+        """
+        This method computes beta values for each streamline
+        Returns:
+            A pd.Series object containing the streamline info with the beta column added
+        """
+        logger.info("Computing beta values for the streamlines")
+        for stream in self.stream_data:
+            for
+
 
     def get_data(self) -> np.ndarray:
         """
