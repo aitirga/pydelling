71eb19ca533880c5f402503fb0b2fd46326720cc
aitirga <aitirga@gmail.com>
2021-03-08 15:11:47 +0100

    feat: add rotated mesh for KORAD model (new geometry)

--- PyFLOTRAN/readers/StreamlineReader.py
+++ PyFLOTRAN/readers/StreamlineReader.py
@@ -8,13 +8,18 @@ from PyFLOTRAN.utils.utils import aperture_from_a_xy_point
 from PyFLOTRAN.paraview_processor import ParaviewProcessor
 import logging
 import pandas as pd
+import pickle
 from pandas.core.groupby import DataFrameGroupBy
+from tqdm import tqdm
+
 logger = logging.getLogger(__name__)
+
 
 class StreamlineReader(BaseReader):
     data: pd.DataFrame
     raw_data: pd.DataFrame
     stream_data: DataFrameGroupBy
+    is_aperture_zero: bool = False
 
     def __init__(self, filename, header=False):
         self.header = None
@@ -51,32 +56,63 @@ class StreamlineReader(BaseReader):
         temp_series: pd.Series = temp_df.groupby("SeedIds").max()["IntegrationTime"]
         return temp_series
 
-    def compute_beta(self):
+    def compute_beta(self, aperture_field: str = None):
         """
         This method computes beta values for each streamline
         Returns:
             A pd.Series object containing the streamline info with the beta column added
         """
+        aperture_field_file = aperture_field if aperture_field else config.beta_integrator.aperture_field_file if config.beta_integrator.aperture_field_file else None
+        assert aperture_field_file, "Define a file containing an aperture field matrix"
+        # Read aperture field
+        with open(aperture_field_file, "rb") as opened_file:
+            aperture_field = pickle.load(opened_file)
+        logger.info(f"Aperture field has been loaded from {aperture_field_file}")
         logger.info("Computing beta values for the streamlines")
-        for stream in self.stream_data.groups:
+        stream_beta = []
+        for stream in tqdm(self.stream_data.groups):
             stream_data = self.stream_data.get_group(stream)
-            stream_beta = self.integrate_beta(stream=stream_data)
-            print(f"Computed beta for stream {stream}")
+            stream_beta_value = self.integrate_beta(stream=stream_data, aperture_field=aperture_field)
+            if self.is_aperture_zero:
+                self.is_aperture_zero = False
+                continue
+            else:
+                stream_beta.append(stream_beta_value)
+            # print(f"Computed beta for stream {stream}")
+        print(stream_beta)
+        return pd.DataFrame(np.array(stream_beta))
 
-    def integrate_beta(self, stream: pd.DataFrame):
+    def integrate_beta(self, stream: pd.DataFrame, aperture_field: np.ndarray):
         """
         This method integrates a given streamline to compute the value of beta
         Args:
             stream: A pandas DataFrame containing the data of a given streamline
-
+            aperture_field: A matrix containing the information of the aperture field
         Returns:
 
         """
         beta = 0.0
+        aperture_field_nx = aperture_field.shape[1]
+        aperture_field_ny = aperture_field.shape[0]
         for index, fragment in stream.iterrows():
             # aperture = aperture_from_a_xy_point(x_point=)
-            aperture = 1.0
-            beta += 2 * fragment["IntegrationTime"] / aperture
+            # Nearest neighbour
+            index_x = int(np.floor(fragment["x"] / config.beta_integrator.dimension_x * aperture_field_nx))
+            index_y = int(np.floor(fragment["y"] / config.beta_integrator.dimension_y * aperture_field_ny))
+            index_row = aperture_field_ny - index_y - 1
+            index_column = index_x
+            if index_row == aperture_field_ny:
+                index_row -= 1
+            if index_column == aperture_field_nx:
+                index_column -= 1
+            # print(fragment["x"], fragment["y"], index_x, index_y, index_row, index_column)
+            # print(index_row, index_column, index_x, index_y)
+            aperture = aperture_field[index_row, index_column]
+            if aperture == 0.0:
+                self.is_aperture_zero = True
+                logger.warning(f"Zero value aperture has been detected on point [{fragment['x']}, {fragment['y']}]")
+                continue
+            beta += 2 * fragment["IntegrationTime"] / aperture / (365 * 24 * 3600)
         return beta
 
     def get_data(self) -> np.ndarray:
@@ -96,4 +132,3 @@ class StreamlineReader(BaseReader):
         # self.data.to_csv(output_file, delimiter=delimiter)
         self.data.to_csv(output_file)
         print(f"The data has been properly exported to the {output_file} file")
-
