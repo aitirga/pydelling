264cb9ea59a6ce112e4d8087fae2f4c0b1189dab
aitirga <aitirga@gmail.com>
2021-03-23 13:15:57 +0100

    feat: add basic unittests for iGPReader and Streamline reader

--- PyFLOTRAN/tests/readers/test_iGPReader.py
+++ PyFLOTRAN/tests/readers/test_iGPReader.py
@@ -1,12 +1,21 @@
 import unittest
 from PyFLOTRAN.readers import iGPReader
+from PyFLOTRAN.config import config
+from PyFLOTRAN.utils import test_data_path
+import logging
+logging.getLogger().setLevel("CRITICAL")
 
 
 class iGPReaderCase(unittest.TestCase):
     """
     This class tests the iGPReader[PyFLOTRAN.readers.iGPReader.io.iGPReader.iGPReader] class implementation
     """
-    def test_something(self):
+    def setUp(self) -> None:
+        self.stream_line_reader = iGPReader(test_data_path() / "test_implicit_to_explicit.gid",
+                                            project_name="test-implicit_to_explicit"
+                                            )
+
+    def test_implicit_to_explicit(self):
         self.assertEqual(True, True)
 
 
