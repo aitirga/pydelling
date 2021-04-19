08677a0394532b6fa043ac6cd58268ce0874d37b
aitirga <aitirga@gmail.com>
2021-04-19 14:17:56 +0200

    feat: add reader for connectflow mesh and bounds method

--- PyFLOTRAN/readers/ConnectFlowReader.py
+++ PyFLOTRAN/readers/ConnectFlowReader.py
@@ -5,31 +5,37 @@ import numpy as np
 import logging
 from PyFLOTRAN.readers import BaseReader
 logger = logging.getLogger(__name__)
-import Ofpp
 from PyFLOTRAN.config import config
 import logging
 from pathlib import Path
-
+import pandas as pd
 logger = logging.getLogger(__name__)
+from linecache import getline, getlines
 
 
 class ConnectFlowReader(BaseReader):
     def __init__(self, filename=None):
         self.filename = Path(filename) if filename else Path(config.open_foam_reader.filename)
-        logger.info(f"Reading OpenFOAM mesh file from {self.filename}")
+        logger.info(f"Reading ConnectFlow mesh file from {self.filename}")
         super().__init__(filename=self.filename)
 
-
     def open_file(self, filename):
-        self.mesh = Ofpp.FoamMesh(self.filename)
+        filename_string = str(filename)
+        header = getline(filename_string, 1).split()
+        self.n_nodes = int(header[1])
+        self.n_elements = int(header[3])
+        self.n_mat = int(header[5])
+        with open(filename, "r") as opened_file:
+            _nodes = [line.rstrip().split() for line in opened_file.readlines()[2: 2 + self.n_nodes - 1]]
+            self._mesh_nodes = pd.DataFrame(np.array(_nodes).astype(np.float), columns=["index", "x", "y", "z"])
+            self._mesh_nodes = self.mesh_nodes.set_index("index")
+
 
     @property
-    def cell_centers(self) -> np.array:
-        if not config.globals.is_cell_centers_read:
-            logger.info(f"Reading cell center locations from {self.filename / '0/C'}")
-            self.mesh.read_cell_centres(self.filename / "0/C")
-            config.globals.is_cell_centers_read = True
-        return self.mesh.cell_centres
+    def mesh_nodes(self) -> pd.DataFrame:
+        assert hasattr(self, "_mesh_nodes"), "Mesh has not been properly assigned"
+        return self._mesh_nodes
+
 
     def get_data(self) -> np.ndarray:
         """
@@ -43,5 +49,19 @@ class ConnectFlowReader(BaseReader):
         Generates a dictionary containing the basic info of the read data
         :return:
         """
-        self.info = {}
+        min_x = self._mesh_nodes["x"].min()
+        max_x = self._mesh_nodes["x"].max()
+        min_y = self._mesh_nodes["y"].min()
+        max_y = self._mesh_nodes["y"].max()
+        min_z = self._mesh_nodes["z"].min()
+        max_z = self._mesh_nodes["z"].max()
+        self.info = {
+            "bounds": {
+                "x": [min_x, max_x],
+                "y": [min_y, max_y],
+                "z": [min_z, max_z],
+            }
+        }
 
+    def get_bounds(self):
+        return self.info["bounds"]
