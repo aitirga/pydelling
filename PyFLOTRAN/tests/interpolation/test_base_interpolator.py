3622cf71e2b12bfeee91efa66634a3a3d05f180f
aitirga <aitirga@gmail.com>
2021-05-10 21:54:52 +0200

    bugfix: fix issues to run the tests properly

--- PyFLOTRAN/tests/interpolation/test_base_interpolator.py
+++ PyFLOTRAN/tests/interpolation/test_base_interpolator.py
@@ -8,7 +8,7 @@ import numpy
 class BaseInterpolatorCase(unittest.TestCase):
     def setUp(self) -> None:
         self.mesh_data = iGPReader(test_data_path() / "test_implicit_to_explicit.gid", project_name="test").get_mesh()
-        self.interpolation_data = CentroidReader(test_data_path() / "centroid_reader_data.csv", split_key=",", header=True).get_data()
+        self.interpolation_data = CentroidReader(test_data_path() / "centroid_reader_data.csv", separator=",", header=True).get_data()
         self.base_interpolator = BaseInterpolator(mesh_data=self.mesh_data, interpolation_data=self.interpolation_data)
 
     def test_set_up(self):
@@ -17,6 +17,7 @@ class BaseInterpolatorCase(unittest.TestCase):
 
     def test_interpolation(self):
         interpolated_data = self.base_interpolator.interpolate()
+        print(interpolated_data)
         numpy.testing.assert_array_equal(interpolated_data, self.mesh_data[:, 0])
 
 
--- PyFLOTRAN/tests/interpolation/test_sparse_data_interpolator.py
+++ PyFLOTRAN/tests/interpolation/test_sparse_data_interpolator.py
@@ -8,7 +8,7 @@ import numpy
 class SparseDataInterpolatorCase(unittest.TestCase):
     def setUp(self) -> None:
         self.mesh_data = iGPReader(test_data_path() / "test_implicit_to_explicit.gid", project_name="test").get_mesh()
-        self.interpolation_data = CentroidReader(test_data_path() / "centroid_reader_data.csv", split_key=",", header=True).get_data()
+        self.interpolation_data = CentroidReader(test_data_path() / "centroid_reader_data.csv", separator=",", header=True).get_data()
         self.base_interpolator = SparseDataInterpolator(mesh_data=self.mesh_data, interpolation_data=self.interpolation_data)
 
     def test_set_up(self):
