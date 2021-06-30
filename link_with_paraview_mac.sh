8587e57797c243223577495a3524d086c105287e
Aitor <aitirga@gmail.com>
2021-06-30 17:49:44 +0200

    feat: add stl files and new mesh creation script

---
+++ link_with_paraview_mac.sh
@@ -0,0 +1,4 @@
+export DYLD_FALLBACK_LIBRARY_PATH='/Applications/ParaView-5.9.1.app/Contents/Libraries'
+export LD_LIBRARY_PATH='/Applications/ParaView-5.9.1.app/Contents/Libraries'
+export DYLD_FALLBACK_FRAMEWORK_PATH='/Applications/ParaView-5.9.1.app/Contents/Frameworks'
+export PYTHONPATH=/Applications/ParaView-5.9.1.app/Contents/Python:/Applications/ParaView-5.9.1.app/Contents/Libraries
