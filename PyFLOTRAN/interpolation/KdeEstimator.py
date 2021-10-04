4b5646a1d6bff97f4861678281a5b4c84b0e9c09
Aitor <aitirga@gmail.com>
2021-10-04 20:31:00 +0200

    feat: add kde training cases

--- PyFLOTRAN/interpolation/KdeEstimator.py
+++ PyFLOTRAN/interpolation/KdeEstimator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.neighbors import KernelDensity
+from scipy.stats.kde import gaussian_kde
 from PyFLOTRAN.utils.decorators import set_run
 import numpy as np
 import logging
@@ -14,10 +15,11 @@ class KdeEstimator:
     """
     is_run: bool
     kde_estimator: KernelDensity
-    def __init__(self, data:pd.DataFrame=None, kernel='gaussian', bandwidth=1000):
+    def __init__(self, data:pd.DataFrame=None, kernel='gaussian', bandwidth=1000, package='scikit'):
         self.data = data
         self.kernel = kernel
         self.bandwidth = bandwidth
+        self.package = package
         if type(self.data) == pd.Series:
             self.data = pd.DataFrame(self.data)
 
@@ -28,20 +30,54 @@ class KdeEstimator:
         """
         if data:
             self.data = data
+        logger.info(f'Fitting KDE generator for {self.data.columns.to_list()} variables')
         self.kde_estimator: KernelDensity = KernelDensity(kernel=self.kernel, bandwidth=self.bandwidth).fit(self._training_data)
-        assert self.kde_estimator, 'Provide some data to work with'
         return self.kde_estimator
 
-    def plot_1D(self, variable=None, n=100, savefig=None):
+    def plot_1d_comparison_histograms(self, variable=None, n=10000, savefig=None, bins=30):
+        plt.clf()
+        fig, ax = plt.subplots()
+        fig: plt.Figure
+        ax: plt.Axes
+        if not variable:
+            variable = self.data.columns[0]
+
+        sampled_values = pd.DataFrame(self.kde_estimator.sample(n), columns=self.data.columns)
+
+        ax.hist(self.data[variable].values, bins=bins,
+                density=True,
+                alpha=0.5,
+                label=variable,
+                )
+
+        ax.hist(sampled_values[variable].values, bins=bins,
+                density=True,
+                alpha=0.5,
+                label=f"{variable}-kde",
+                )
+
+        plt.legend()
+        if savefig:
+            plt.savefig(savefig)
+        else:
+            plt.show()
+        return ax
+
+    def plot_1d(self, variable=None, n=100, savefig=None):
         """
         Plots the trained KDE using the original data bounds
         Returns: Axes object of the generated plot
         """
+        plt.clf()
         if not variable:
             variable = self.data.columns[0]
+        logger.info(f'Generating 1D comparison plot for {variable} variable')
 
-        sampling_data = np.arange(self.data[variable].min(), self.data[variable].max(), n)
-        predicted_distribution = self.kde_estimator.score_samples(sampling_data.reshape(-1, 1))
+        temp = [np.linspace(self.data[column].min(), self.data[column].max(), n) for column in self.data]
+        sampling_data = np.meshgrid(*temp)
+        sampling_data: np.ndarray = np.vstack(map(np.ravel, sampling_data)).T.reshape(-1, self.data.shape[1])
+
+        predicted_distribution = self.kde_estimator.score_samples(sampling_data)
         predicted_distribution = np.exp(predicted_distribution)
 
         # Plot result
