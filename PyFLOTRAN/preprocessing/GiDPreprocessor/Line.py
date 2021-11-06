e8e438b6eb640e3c98169c78c30d5572c2bd4739
Aitor <aitirga@gmail.com>
2021-11-06 19:40:41 +0100

    feat: add extrusion and surface objects to GiD processor

--- PyFLOTRAN/preprocessing/GiDPreprocessor/Line.py
+++ PyFLOTRAN/preprocessing/GiDPreprocessor/Line.py
@@ -1,33 +1,61 @@
+from __future__ import annotations
 import numpy as np
 from ._AbstractGidObject import _AbstractGidObject
-from functools import wraps
 from .Point import Point
 import logging
-
+from typing import List
 logger = logging.getLogger(__file__)
-
 
 class Line(_AbstractGidObject):
     local_id: int = 1
     global_id: int = 1
+    lines: List = []
+    has_copy = False
     def __init__(self, point_1: Point, point_2: Point):
         self.local_id = Line.local_id
         self.id = None
+        if np.array_equal(point_1.coords, point_2.coords):
+            raise ValueError('The start and end points of a line cannot be equal')
         self.point_1 = point_1
         self.point_2 = point_2
+        self.points = [self.point_1, self.point_2]
         Line.local_id += 1
 
     def add(self):
+        # Check if line already exists
+        for line in Line.lines:
+            if Line.check_lines_equal(self, line):
+                self.id = line.id
+                self.has_copy = True
+                return ''
+
         self.id = Line.global_id
-        logger.info(f'Adding line {self.id} connecting {self.point_1} and {self.point_2}')
         Line.global_id += 1
+        Line.lines.append(self)
+        logger.info(f'Adding line {self.id} connecting {self.point_1} and {self.point_2}')
         export_str = 'Mescape Geometry Create Line\n'
         export_str += 'Join\n'
         export_str += f'{self.point_1.id}\n'
         export_str += f'{self.point_2.id}\n'
         return export_str
 
+    @staticmethod
+    def check_lines_equal(line_1: Line, line_2: Line):
+        point_1_coords = [line_1.point_1.coords.tolist(), line_1.point_2.coords.tolist()]
+        if line_2.point_1.coords.tolist() in point_1_coords and line_2.point_2.coords.tolist() in point_1_coords:
+            return True
+        else:
+            return False
+
     def __repr__(self):
-        return f"Line {self.local_id}"
+        if self.id:
+            return f"Line {self.id}"
+        else:
+            return f"Line (local) {self.local_id}"
+
+
+
+
+
 
 
---
+++ PyFLOTRAN/preprocessing/GiDPreprocessor/Surface.py
@@ -0,0 +1,48 @@
+import numpy as np
+from ._AbstractGidObject import _AbstractGidObject
+from .Line import Line
+from .Point import Point
+import logging
+from typing import List
+logger = logging.getLogger(__file__)
+
+
+class Surface(_AbstractGidObject):
+    local_id: int = 1
+    global_id: int = 1
+    surfaces: List = []
+
+    def __init__(self, lines: List[Line]):
+        self.local_id = Surface.local_id
+        self.lines = lines
+        self.id = None
+        Surface.local_id += 1
+
+    def add(self):
+        self.id = Surface.global_id
+        logger.info(f'Adding surface {self.id} connecting {self.lines}')
+        Surface.global_id += 1
+        Surface.surfaces.append(self)
+        export_str = 'Mescape Geometry Create NurbsSurface\n'
+        for line in self.lines:
+            export_str += f'{line.id} '
+        export_str += '\n'
+        return export_str
+
+    def extrude(self, start_point: Point, end_point: Point, end_object):
+        logger.info(f'Extruding {self} using direction {start_point.coords} -> {end_point.coords}')
+        export_str = f'Mescape Utilities Copy Surfaces DoExtrude {end_object} MantainLayers Translation FNoJoin {start_point.coords_comma} FNoJoin {end_point.coords_comma}\n'
+        extra_surfaces = 5
+        extra_lines = 2 * len(self.lines)
+        extra_points = len(self.lines)
+        Surface.global_id += extra_surfaces
+        Line.global_id += extra_lines
+        Point.global_id += extra_points
+        return export_str
+
+    def __repr__(self):
+        if self.id:
+            return f"Surface {self.id}"
+        else:
+            return f"Surface (local) {self.local_id}"
+
