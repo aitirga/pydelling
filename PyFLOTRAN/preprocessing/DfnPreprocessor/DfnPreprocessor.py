14e54e4f16faf01549b5eda22a204d08bb59d5bd
Aitor <aitirga@gmail.com>
2022-01-18 13:00:26 +0100

    feat: add latest changes for fracture visualization

--- PyFLOTRAN/preprocessing/DfnPreprocessor/DfnPreprocessor.py
+++ PyFLOTRAN/preprocessing/DfnPreprocessor/DfnPreprocessor.py
@@ -1,8 +1,12 @@
+import numpy as np
+
 from .Fracture import Fracture
 from typing import List
 import pandas as pd
 import logging
 from tqdm import tqdm
+import plotly.graph_objects as go
+
 
 logger = logging.getLogger(__name__)
 
@@ -50,6 +54,50 @@ class DfnPreprocessor(object):
             z=z,
             size=size
         ))
+
+    def summary(self):
+        """Prints a summary of the dfn object."""
+        pass
+
+    def visualize_dfn(self):
+        """Visualizes the dfn object."""
+        self.generate_dfn_plotly()
+
+    def export_dfn_image(self, filename):
+        pass
+
+    def generate_dfn_plotly(self):
+        ''' Generates a plotly figure of the dfn object.
+
+        Returns: A plotly figure
+
+        '''
+        fig = go.Figure()
+        for fracture in self.dfn:
+            fracture_sides = fracture.get_side_points()
+            print(fracture_sides[:, 0].shape)
+            print(fracture_sides[:, 0])
+
+            fig.add_trace(go.Scatter3d(
+                x=[fracture.x_centroid],
+                y=[fracture.y_centroid],
+                z=[fracture.z_centroid],
+            ))
+
+            fig.add_trace(go.Mesh3d(
+                x=fracture_sides[:, 0],
+                y=fracture_sides[:, 1],
+                z=fracture_sides[:, 2],
+                # line=dict(
+                #     color='rgb(0, 0, 0)',
+                #     width=2
+                # )
+            ))
+
+            break
+        fig.show()
+
+
 
 
 
--- PyFLOTRAN/preprocessing/DfnPreprocessor/Fracture.py
+++ PyFLOTRAN/preprocessing/DfnPreprocessor/Fracture.py
@@ -1,9 +1,58 @@
+import numpy as np
+
 
 class Fracture(object):
     def __init__(self, dip, dip_dir, x, y, z, size):
         self.dip = dip
         self.dip_dir = dip_dir
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x_centroid = x
+        self.y_centroid = y
+        self.z_centroid = z
         self.size = size
+        self.centroid = np.array([self.x_centroid, self.y_centroid, self.z_centroid])
+
+
+    def get_side_points(self):
+        """
+        alpha = 45; %strike
+        delta = 30; %dip
+        w = 4; %width (dipping side)
+        L = 9; %Length (side parallel to the surface)
+
+        A = [0,0,0]; % Pivot point at the surface
+
+        H = w*sind(alpha+90);
+        V = w*cosd(alpha+90);
+        Z = -w*sind(delta);
+
+        B = [L*sind(alpha) L*cosd(alpha) 0]+A;
+        C = [L*cosd(90-alpha)+H L*sind(90-alpha)+V Z]+A;
+        D = [w*sind(alpha+90) w*cosd(alpha+90) -w*sind(delta)]+A;
+
+        P = [A; B; C; D];
+        """
+        alpha = self.dip_dir / 360 * (2 * np.pi)
+        delta = self.dip / 360 * (2 * np.pi)
+        w = self.size
+        L = self.size
+
+        A = np.array(self.centroid)
+        A[0] = A[0] - np.sin(alpha) * L / 2
+        A[1] = A[1] + np.cos(alpha) * L / 2
+        A[2] = A[2] + np.sin(delta) * L / 2
+
+
+        H = w * np.sin(alpha + np.pi / 2)
+        V = w * np.cos(alpha + np.pi / 2)
+        Z = -w * np.sin(delta)
+
+        B = np.array([L * np.sin(alpha), L * np.cos(alpha), 0]) + A
+        C = np.array([L * np.cos(np.pi / 2 - alpha) + H, L * np.sin(np.pi / 2 - alpha) + V, Z]) + A
+        D = np.array([w * np.sin(alpha + np.pi / 2), w * np.cos(alpha + np.pi / 2), -w * np.sin(delta)]) + A
+
+        P = np.array([A, B, C, D])
+
+        return P
+
+
+
