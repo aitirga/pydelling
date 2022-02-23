4b68a6fcfa4d1f1da26a2ce7f496e7e959892677
Aitor <aitirga@gmail.com>
2022-02-23 15:32:08 +0100

    feat: add simple logic to MeshPreprocessor

--- PyFLOTRAN/preprocessing/MeshPreprocessor/MeshPreprocessor.py
+++ PyFLOTRAN/preprocessing/MeshPreprocessor/MeshPreprocessor.py
@@ -1,6 +1,35 @@
+import geometry as geometry
+from typing import *
+import numpy as np
 
 
 class MeshPreprocessor(object):
     """Contains the logic to preprocess and work with a generic unstructured mesh"""
+    elements: List[geometry.BaseElement]
+    nodes: List[np.ndarray]
+    centroids: List[np.ndarray]
+
     def __init__(self):
-        pass
+        self.nodes = []
+        self.elements = []
+        self.centroids = []
+
+    def add_element(self, element: geometry.BaseElement):
+        self.elements.append(element)
+
+    def add_node(self, node: np.ndarray):
+        self.nodes.append(node)
+
+    @property
+    def n_nodes(self):
+        """Returns the number of nodes in the mesh"""
+        return len(self.nodes)
+
+    @property
+    def n_elements(self):
+        """Returns the number of elements in the mesh"""
+        return len(self.elements)
+
+
+
+
