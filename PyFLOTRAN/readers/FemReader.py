f9c5ff33e2e7db278bc793f33a36f9dfe3d19d75
andreapalacios01 <andrea.palacios@amphos21.com>
2022-02-24 11:16:39 +0100

    Creation of "FemReader" class, without final nomenclature

--- PyFLOTRAN/readers/FemReader.py
+++ PyFLOTRAN/readers/FemReader.py
@@ -1,4 +1,7 @@
+import os
+import numpy as np
 from ..preprocessing.MeshPreprocessor import MeshPreprocessor
+
 
 
 class FemReader(MeshPreprocessor):
@@ -6,4 +9,49 @@ class FemReader(MeshPreprocessor):
         super().__init__()
 
     def open_file(self, filename, **kwargs):
-        pass
+        
+        with open("r", filename) as f:
+            for line in f:
+                line = line.rstrip()
+                split_line = line.split()
+
+                if split_line[0] == "DIMENS":
+                    line = f.readline()
+                    line = line.rstrip()
+                    split_line = line.split()
+                    self.n_elements = int(split_line[0])
+                    self.n_nodes = int(split_line[1])
+
+                    print(line)
+
+                elif split_line[0] == "VARNODE":
+                    line = f.readline()
+                    line = line.rstrip()
+                    split_line = line.split()
+                    self.n_elem_per_node = int(split_line[1])
+                    self.nodes_elem = np.zeros([self.n_node, self.n_elem_per_node])
+
+                    for n in range(0, self.n_node):
+                        line = f.readline()
+                        line = line.rstrip()
+                        split_line = line.split()
+                        for ne in range(0, self.n_elem_per_node):
+                            self.nodes_elem[n, ne] = int(split_line[ne + 1])
+
+                elif split_line[0] == "XYZCOOR":
+                    line = f.readline()
+                    line = line.rstrip()
+                    split_line = line.split()
+                    self._mesh_nodes = np.zeros([self.n_elements, 3])
+                    for e in range(0, self.n_elements):
+                        self._mesh_nodes[e, 0] = float(split_line[0][0:-1])
+                        self._mesh_nodes[e, 1] = float(split_line[1][0:-1])
+                        self._mesh_nodes[e, 2] = float(split_line[2][0:-1])
+                        line = f.readline()
+                        line = line.rstrip()
+                        split_line = line.split()
+                    break
+
+                else:
+                    pass
+        
