186b340985b0e3118ba6faaa93158fa316a3215b
Aitor <aitirga@gmail.com>
2022-03-03 15:39:13 +0100

    feat: add base structure of the UnstructuredMeshReader class

---
+++ PyFLOTRAN/readers/UnstructuredMeshReader.py
@@ -0,0 +1,8 @@
+from PyFLOTRAN.preprocessing.MeshPreprocessor import MeshPreprocessor
+
+
+class UnstructuredMeshReader(MeshPreprocessor):
+    def __init__(self, nodes, vertices):
+        super().__init__()
+        self.nodes = nodes
+        self.vertices = vertices
