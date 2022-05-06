55565edee0ca64547e3af377780a97c2304a94ac
andreapalacios01 <andrea.palacios@amphos21.com>
2022-05-06 12:34:58 +0200

    Add unit test for upscaling

--- PyFLOTRAN/tests/preprocessing/DfnPreprocessor/test_upscaling.py
+++ PyFLOTRAN/tests/preprocessing/DfnPreprocessor/test_upscaling.py
@@ -10,25 +10,48 @@ class UpscalingCase(unittest.TestCase):
     def test_upscale_hexahedra(self):
         dfn_preprocessor = DfnPreprocessor()
         dfn_preprocessor.add_fracture(
-            x=0.1, y=0.1, z=0.0, dip=20, dip_dir=50, size=1.2
+            x=0.0, y=0.0, z=0.0, dip=0, dip_dir=0, size=2.0
         )
+        dfn_preprocessor.add_fracture(
+            x=0.1, y=0.0, z=0.1, dip=0, dip_dir=0, size=0.5
+        )
+        dfn_preprocessor[0].aperture = 0.01
+        dfn_preprocessor[1].aperture = 0.01
+
+        dfn_preprocessor.to_obj('fracture.obj')
         mesh_preprocessor = MeshPreprocessor()
-        mesh_preprocessor.add_tetrahedra(
-            node_ids=np.array([0, 1, 2, 3]),
+        mesh_preprocessor.add_hexahedra(
+            node_ids=np.array([0, 1, 2, 3, 4, 5, 6, 7]),
             node_coords=[
-                np.array([0.0, 0.0, -0.5]),
-                np.array([1.0, 0.0, -0.5]),
-                np.array([0.0, 1.0, -0.5]),
-                np.array([0.0, 0.0, 0.5]),
+                np.array([-0.5, -0.5, -0.5]),
+                np.array([0.5, -0.5, -0.5]),
+                np.array([0.5, 0.5, -0.5]),
+                np.array([-0.5, 0.5, -0.5]),
+                np.array([-0.5, -0.5, 0.5]),
+                np.array([0.5, -0.5, 0.5]),
+                np.array([0.5, 0.5, 0.5]),
+                np.array([-0.5, 0.5, 0.5])
             ],
         )
-        # Intersect fracture with mesh
-        intersected_points = mesh_preprocessor.elements[0].intersect_faces_with_plane(
-            dfn_preprocessor[0].plane)
 
-        solution = 1.00
-        self.assertEqual(1.0, solution)
-        # nptest.assert_array_almost_equal(intersected_points, solution)
+        dfn_upscaler = DfnUpscaler(
+            dfn=dfn_preprocessor,
+            mesh=mesh_preprocessor,
+            save_intersections=True,
+        )
+
+        # Upscale porosity
+        porosity = dfn_upscaler.upscale_mesh_porosity()
+
+        # Upscale permeability
+
+        permeability = dfn_upscaler.upscale_mesh_permeability()
+
+        porosity_solution = 0.0125
+        #self.assertEqual(porosity, porosity_solution)
+        self.assertAlmostEqual(porosity[0], porosity_solution)
+        permeability_solution = np.array([[1.147004,0,0],[0,1.147004,0],[0,0,0]])
+        nptest.assert_array_almost_equal(permeability[0], permeability_solution)
 
 
 
