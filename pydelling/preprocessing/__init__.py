3b4bfb9679bc97b7998bbf6fbd04c13b6013e0c5
Aitor <aitirga@gmail.com>
2022-08-27 02:14:45 +0200

    refactor: update requirements.txt for new release

--- pydelling/preprocessing/__init__.py
+++ pydelling/preprocessing/__init__.py
@@ -1,5 +1,5 @@
 from .BasePreprocessing import BasePreprocessing
-from .STLFromPointCloud import STLFromPointCloud
+# from .STLFromPointCloud import STLFromPointCloud
 from .dfn_preprocessor import DfnPreprocessor
 from .mesh_preprocessor import MeshPreprocessor
 
